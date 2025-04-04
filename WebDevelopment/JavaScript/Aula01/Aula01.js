30d824f012c3a925963ca2a11e25cd33bb22aef0
GuilhermeSM <guilhermesm.0808@gmail.com>
2025-04-03 22:42:42 -0300

    Aula 01 de JavaScript

---
+++ WebDevelopment/JavaScript/Aula01/Aula01.js
@@ -0,0 +1,23 @@
+// window.alert("Hello World!")
+
+// let nomeCompleto = "Guilherme Satler Macedo"
+
+let number = 1
+
+let correct = true
+
+let space = null
+
+let nome = prompt("What's your name?", "Insira aqui seu nome")
+
+alert(nome)
+
+console.log(typeof nome)
+
+let result = confirm("Would you like to proceed?")
+
+console.log(result)
+console.log(typeof result)
+
+console.log("Hello " + nome + ". How's it going?")
+console.log (`Hello ${nome}, how's it going? `)
---
+++ WebDevelopment/JavaScript/Aula01/Exercicio01.js
@@ -0,0 +1,12 @@
+let name = prompt("What's your name?", "Insert your name.")
+let age = prompt("How old are you?", "Insert your age")
+let work = prompt("What do you work with?","Insert your job title")
+
+alert(`Hello ${name}, you're ${age} years old and you work as an ${work}.`)
+
+let numero1 = Number(prompt("Choose a number to be added to another: "))
+let numero2 = Number(prompt("Choose a number to be added to another: "))
+
+let resultado = numero1 + numero2
+
+console.log(resultado)
