3c1e1bfd64da07c42a61dbdc690e8e57b8ca310e
GuilhermeSM <guilhermesm.0808@gmail.com>
2025-09-23 21:54:15 -0300

    23/09

---
+++ testing.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import datetime as dt
+import pandas as pd
+
+lista_dias = []
+
+today = dt.datetime.now()
+today = today.date()
+
+try:
+    for i in range(1, 33):
+        ultimo_dia = dt.date(2025, 10, i)
+        lista_dias.append(str(ultimo_dia))
+except:
+    print(f"Ultimo dia do mes: {i-1}")
+
+print(ultimo_dia)
+print(lista_dias)
+
